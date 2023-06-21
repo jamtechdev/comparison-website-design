793cbbb9a88c61ae778cfb560aacc4a227b542e5
jamtechdev <jamtechba@gmail.com>
2023-06-21 19:13:09 +0530

    Changes done

--- src/app/product-page/page.jsx
+++ src/app/product-page/page.jsx
@@ -288,14 +288,19 @@ export default function ProductPage() {
                           <div className="spec-col">
                             <h5>
                               Power
-                              <span>
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  viewBox="0 0 24 24"
-                                >
-                                  <path d="M12 19C12.8284 19 13.5 19.6716 13.5 20.5C13.5 21.3284 12.8284 22 12 22C11.1716 22 10.5 21.3284 10.5 20.5C10.5 19.6716 11.1716 19 12 19ZM12 2C15.3137 2 18 4.68629 18 8C18 10.1646 17.2474 11.2907 15.3259 12.9231C13.3986 14.5604 13 15.2969 13 17H11C11 14.526 11.787 13.3052 14.031 11.3989C15.5479 10.1102 16 9.43374 16 8C16 5.79086 14.2091 4 12 4C9.79086 4 8 5.79086 8 8V9H6V8C6 4.68629 8.68629 2 12 2Z"></path>
-                                </svg>
-                              </span>
+                              <div className="hover_container">
+                                <span>
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                  >
+                                    <path d="M12 19C12.8284 19 13.5 19.6716 13.5 20.5C13.5 21.3284 12.8284 22 12 22C11.1716 22 10.5 21.3284 10.5 20.5C10.5 19.6716 11.1716 19 12 19ZM12 2C15.3137 2 18 4.68629 18 8C18 10.1646 17.2474 11.2907 15.3259 12.9231C13.3986 14.5604 13 15.2969 13 17H11C11 14.526 11.787 13.3052 14.031 11.3989C15.5479 10.1102 16 9.43374 16 8C16 5.79086 14.2091 4 12 4C9.79086 4 8 5.79086 8 8V9H6V8C6 4.68629 8.68629 2 12 2Z"></path>
+                                  </svg>
+                                </span>
+                                <p className="display-content">
+                                Battery power, or battery capacity, represents the amount of electrical energy that a battery can store. More battery power can be an indication of longer battery life.
+                                </p>
+                              </div>
                             </h5>
                           </div>
                           <div className="spec-col">
@@ -1133,8 +1138,23 @@ export default function ProductPage() {
             <Col md={8} lg={8}>
               <p className="review-content">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry standard dummy
+                industry. Lorem Ipsum has been the industry standard dummy text
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Lorem Ipsum is simply dummy text of the printing
+                and typesetting industry. Lorem Ipsum has been the industry
+                standard dummy text Ipsum has been the industry standard dummy
+                text
+                <br />
+                <br />
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry standard dummy text
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Lorem Ipsum is simply dummy text of the printing
+                and typesetting industry. Lorem Ipsum has been the industry
+                standard dummy text Ipsum has been the industry standard dummy
                 text Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the industry standard
+                dummy text Lorem Ipsum is simply dummy text of the printing and
                 typesetting industry. Lorem Lorem Ipsum is simply dummy text of
                 the printing and typesetting industry. Lorem Ipsum has been the
                 industry standard dummy text Ipsum has been the industry
@@ -1142,27 +1162,12 @@ export default function ProductPage() {
                 <br />
                 <br />
                 Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry standard dummy
-                text Lorem Ipsum is simply dummy text of the printing and
-                typesetting industry. Lorem Lorem Ipsum is simply dummy text of
-                the printing and typesetting industry. Lorem Ipsum has been the
-                industry standard dummy text Ipsum has been the industry
-                standard dummy text Lorem Ipsum is simply dummy text of the
-                printing and typesetting industry. Lorem Ipsum has been the
-                industry standard dummy text Lorem Ipsum is simply dummy text
-                of the printing and typesetting industry. Lorem Lorem Ipsum is
-                simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry standard dummy text Ipsum
-                has been the industry standard dummy text
-                <br />
-                <br />
+                industry. Lorem Ipsum has been the industry standard dummy text
                 Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry standard dummy
-                text Lorem Ipsum is simply dummy text of the printing and
-                typesetting industry. Lorem Lorem Ipsum is simply dummy text of
-                the printing and typesetting industry. Lorem Ipsum has been the
-                industry standard dummy text Ipsum has been the industry
-                standard dummy text
+                industry. Lorem Lorem Ipsum is simply dummy text of the printing
+                and typesetting industry. Lorem Ipsum has been the industry
+                standard dummy text Ipsum has been the industry standard dummy
+                text
               </p>
               <Row className="mt-3">
                 <Col md={12} lg={6}>
