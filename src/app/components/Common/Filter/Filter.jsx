4991e728f12ac2b798ed016a48fba085d682cd5c
jamtechdev <jamtechba@gmail.com>
2023-07-07 15:50:26 +0530

    Design docx tasks

--- src/app/components/Common/Filter/Filter.jsx
+++ src/app/components/Common/Filter/Filter.jsx
@@ -7,9 +7,11 @@ export default function Filter() {
         <h6>TECHNOLOGY</h6>
         <Accordion className="filter-accordion">
           <Accordion.Item eventKey="0">
-            <Accordion.Header>
-              Mapping Technology <i className="ri-arrow-down-s-fill"></i>
-            </Accordion.Header>
+       
+            <div className="accordion-header">
+            <button type="button" aria-expanded="false" class="accordion-button collapsed">Mapping Technology <i className="ri-arrow-down-s-fill"></i></button>
+              
+            </div>
             <Accordion.Body>
               <Form.Check required label="Febonic" />
               <Form.Check required label="Durian" />
@@ -18,9 +20,13 @@ export default function Filter() {
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="1">
-            <Accordion.Header>
-              Autonomy <i className="ri-arrow-down-s-fill"></i>
-            </Accordion.Header>
+          <div className="accordion-header">
+            <button type="button" aria-expanded="false" class="accordion-button collapsed"> Autonomy <i className="ri-arrow-down-s-fill"></i></button>
+              
+            </div>
+            {/* <Accordion.Header>
+             
+            </Accordion.Header> */}
             <Accordion.Body>
               <Form.Range />
               <div className="range">
@@ -30,9 +36,13 @@ export default function Filter() {
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="2">
-            <Accordion.Header>
-              App Control <i className="ri-arrow-down-s-fill"></i>
-            </Accordion.Header>
+          <div className="accordion-header">
+            <button type="button" aria-expanded="false" class="accordion-button collapsed">  App Control <i className="ri-arrow-down-s-fill"></i></button>
+              
+            </div>
+            {/* <Accordion.Header>
+             
+            </Accordion.Header> */}
             <Accordion.Body>
             <div className="d-flex justify-content-between">
                 <Form.Check required label="yes" />
@@ -50,8 +60,12 @@ export default function Filter() {
         <h6>Features</h6>
         <Accordion className="filter-accordion">
           <Accordion.Item eventKey="0">
+          <div className="accordion-header">
+            <button type="button" aria-expanded="false" class="accordion-button collapsed">   Mapping Technology <i className="ri-arrow-down-s-fill"></i></button>
+              
+            </div>
             <Accordion.Header>
-              Mapping Technology <i className="ri-arrow-down-s-fill"></i>
+             
             </Accordion.Header>
             <Accordion.Body>
               <Form.Check required label="Febonic" />
