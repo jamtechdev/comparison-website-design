7687ff53c962b37bd41bf1f3c6ae1ff7dbc8a5e9
G-cake <girjeshd1234@gmail.com>
2023-10-06 19:01:43 +0530

    options fix yes no in sidebar attributes

--- src/app/_helpers/filter.js
+++ src/app/_helpers/filter.js
@@ -1,4 +1,4 @@
-export const filterArrayOfObject = (obj) => {  
+export const filterArrayOfObject = (obj) => {
     let uniq = []
     // console.log(obj.algorithm);
     if (obj.algorithm == "absolute_value") {
@@ -7,11 +7,20 @@ export const filterArrayOfObject = (obj) => {
                 uniq.push(obj.values[i].name);
             }
         }
-        if(uniq.length>0)
-        return {
-            type: "dropdown",
-            values: uniq
+        if(uniq.includes('no') && !uniq.includes('yes'))
+        {
+            uniq.push('yes');
         }
+        else if(uniq.includes('yes') && !uniq.includes('no'))
+        {
+            uniq.push('no');
+        }
+
+        if (uniq.length > 0)
+            return {
+                type: "dropdown",
+                values: uniq
+            }
     }
     else if (obj.algorithm == "highest_to_lowest" || obj.algorithm == "lowest_to_highest") {
         // console.log(obj.values)
@@ -23,18 +32,18 @@ export const filterArrayOfObject = (obj) => {
         let numberedUniq = uniq.map((ele) => Number(ele))
         let sortedArray = numberedUniq.sort(function (a, b) { return a - b })
         if (sortedArray.length <= 6) {
-            if(sortedArray.length>0)
-            return {
-                type: "dropdown",
-                values: sortedArray
-            }
+            if (sortedArray.length > 0)
+                return {
+                    type: "dropdown",
+                    values: sortedArray
+                }
         }
         else {
             return {
                 type: "range",
                 values: sortedArray,
-                minValue : Math.min(...sortedArray),
-                maxValue : Math.max(...sortedArray)
+                minValue: Math.min(...sortedArray),
+                maxValue: Math.max(...sortedArray)
             }
         }
     }
