e86496f78cf8ffedb696e94459abbccb7a12e941
Vijay Reshamwala <rvijay@alita.local>
2023-10-24 17:57:10 +0530

    fix: remove short code element

--- src/app/hooks/useChart.js
+++ src/app/hooks/useChart.js
@@ -1,7 +1,7 @@
 // hooks/usePieChart.js
 import { useEffect, useState } from "react";
 //import ReactDOM from "react-dom";
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import { graphService } from "../_services/graph.service.js";
 import PiChart from "../_chart/PieChart";
 import PieChart from "../_chart/PieChart";
@@ -16,7 +16,7 @@ const useChart = (pattern, apiEndpoint) => {
     const searchForPattern = async () => {
       const content = document.body.textContent;
       const elementsWithNodeType1 = document.body.querySelectorAll("p");
-      elementsWithNodeType1.forEach(async (element,index) => {
+      elementsWithNodeType1.forEach(async (element, index) => {
         const textContent = element.textContent;
         if (
           textContent.includes(searchPattern) &&
@@ -33,52 +33,37 @@ const useChart = (pattern, apiEndpoint) => {
             if (chartData.length > 0) {
               const plotData = regenerateData(chartData);
               if (plotData && plotData.length > 0) {
-                console.log(plotData)
-                const container = document.createElement('div');
-                element.insertAdjacentElement('afterend', container);
-              //  const container = element;
-                const root = createRoot(container); 
-                root.render(<PiChart
-                      data={plotData}
-                      pieSize={300}
-                      svgSize={300}
-                      innerRadius={90}
-                      containerId={`pie${index}`}
-                    />);
-                // ReactDOM.render(
-                //   <PiChart
-                //     data={plotData}
-                //     pieSize={400}
-                //     svgSize={500}
-                //     innerRadius={100}
-                //     containerId="pie"
-                //   />,
-                //   element
-                // );
+                console.log(plotData);
+                const container = document.createElement("div");
+                element.insertAdjacentElement("afterend", container);
+                const root = createRoot(container);
+                root.render(
+                  <PiChart
+                    data={plotData}
+                    pieSize={300}
+                    svgSize={300}
+                    innerRadius={90}
+                    containerId={`pie${index}`}
+                  />
+                );
+                element.remove()
               }
             }
-            //  console.log(data)
           }
-          //  console.log(element)
-          // console.log(element.parentElement.append('<div>hi india...</div>'))
-          // const data = await graphService.getGraphData({graph_shortcode: "[pie-chart;Robot Vacuum Cleaners;Noisiness:0-80,Can Mop:yes;Dirt sensor]" })
-          // console.log(data)
         }
       });
-      // const chartsElement = document.querySelectorAll(".render-chart");
-      //console.log(chartsElement)
     };
     searchForPattern();
   });
   function regenerateData(data) {
     const chartData = [];
-   // data=[1, 1, 2, 3, 5, 8, 13, 21]
-  
+    //data=[1, 1, 2, 3, 5, 8, 13, 21]
+
     if (data && data.length > 0) {
       data.forEach((val) => {
         chartData.push({
           label: val,
-          value:val,
+          value: val,
         });
       });
     }
