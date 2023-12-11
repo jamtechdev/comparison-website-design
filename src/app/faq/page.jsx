9352893d47a31caaf3b86b5dba7c7796b8aff2eb
Jamtech Dev <jamtechba@gmail.com>
2023-12-11 16:28:05 -0500

    11-12-23 4:28pm

--- src/app/faq/page.jsx
+++ src/app/faq/page.jsx
@@ -1,46 +1,38 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import {  Col, Container, Link, Form, Row, } from "react-bootstrap";
-import { getterService  } from "../_services/getter.service";
-
+import { Col, Container, Link, Form, Row } from "react-bootstrap";
+import { getterService } from "../_services/getter.service";
 
 export default function PrivacyAndPolicy() {
-const[faq, setFaq] = useState({});
+  const [faq, setFaq] = useState({});
 
-useEffect(() => {
-    getterService.getPrivacyPolicy()
-      .then((res) => {
-        setFaq(res.data);
-      })
+  useEffect(() => {
+    getterService.getPrivacyPolicy().then((res) => {
+      setFaq(res.data);
+    });
   }, []);
-
   return (
     <>
-    <section className="breadcrumb-section">
+      <section className="breadcrumb-section">
         <Container>
-            <Row >
-                
-                <Col md={12}>
-                    <h1 className="heading-primary">FAQ</h1>
-                </Col>
-            </Row>
+          <Row>
+            <Col md={12}>
+              <h1 className="heading-primary">FAQ</h1>
+            </Col>
+          </Row>
         </Container>
-    </section>
-    <section className="contentSec pt-3 pb-5">
+      </section>
+      <section className="contentSec pt-3 pb-5">
         <Container>
-            <Row>
-                <Col md={8}>
-                    <div className="contentPara">
-                    <p>
-          {faq?.data?.content || "Loading..."}
-        </p>
-                    </div>
-                </Col>
-                
-            </Row>
+          <Row>
+            <Col md={8}>
+              <div className="contentPara">
+                <p>{faq?.data?.content || "Loading..."}</p>
+              </div>
+            </Col>
+          </Row>
         </Container>
-    </section>
-</>
-  )
-
+      </section>
+    </>
+  );
 }
