2cb7bda69855f952e4606dee6d860ea4efc713ca
Jamtech_Dev <jamtjtplartisan@gmail.com>
2023-12-13 13:52:14 +0530

    elab added in comment

--- src/app/components/Svg/ProsConsToolTip.jsx
+++ src/app/components/Svg/ProsConsToolTip.jsx
@@ -24,11 +24,11 @@ const ProsConsToolTip = (props) => {
                   {hover_phrase}
                 </b>
               </p>
-              {info_not_verified != "" && <hr />}
+              {info_not_verified  && <hr />}
             </>
           )}
 
-          {info_not_verified != "" && (
+          {info_not_verified  && (
             <p class="mb-2">
               <i>
                 (Information is not verified. If you believe this is a mistake,
