4cb3fb9410b452af16931463524bbcfa3eb9eb7e
Jamtech_Dev <jamtjtplartisan@gmail.com>
2023-12-13 18:46:56 +0530

    minor change

--- src/app/components/Svg/ProsConsToolTip.jsx
+++ src/app/components/Svg/ProsConsToolTip.jsx
@@ -1,32 +1,42 @@
 import React from "react";
-
 const ProsConsToolTip = (props) => {
   const { hover_phrase, info_not_verified, data } = props;
+  function replaceSpecialCharacters(sentence, newValues) {
+    // Define an array of special characters to be replaced
+    const specialCharacters = ["@@@", "###", "$$$"];
+
+    // Replace each special character with the corresponding new value
+    specialCharacters.forEach((char, index) => {
+      sentence = sentence?.replace(new RegExp(char, "g"), newValues[index]);
+    });
+
+    return sentence;
+  }
+  const originalSentence = hover_phrase;
+  const newValues = [
+    data?.product_name,
+    data?.value,
+    `is better than ${data?.is_better_than} % of ${data?.attribute_name} and is same as ${data?.is_same_as} % of ${data?.attribute_name}`,
+  ];
+  const updatedSentence = replaceSpecialCharacters(originalSentence, newValues);
   return (
     <>
       <div className="tooltip-display-content">
         <p class="mb-2">
           <b>
             {[
-              data?.product_name,
-              data?.value,
-              data?.value,
-              <>
-                <span style={{ color: "rgb(9, 54, 115)" }}>
-                  {" "}
-                  is better than {(data?.is_better_than)*100} %{" "}
-                </span>{" "}
-                of {data?.attribute_name} and is same as{" "}
-                <span style={{ color: "rgb(9, 54, 115)" }}>
-                  {" "}
-                  {(data?.is_same_as)*100} %{" "}
-                </span>{" "}
-                of {data?.attribute_name}
-              </>,
+              data?.product_name ,
+              data?.value * 100,
+              ` is better than ${
+                data?.is_better_than ? data?.is_better_than : "N/A"
+              } % of ${
+                data?.attribute_name ? data?.attribute_name : "N/A"
+              } and is same as ${
+                data?.is_same_as ? data?.is_same_as * 100 : ""
+              } % of ${data?.attribute_name ? data?.attribute_name : "N/A"}`,
             ]}
           </b>
         </p>
-
         {info_not_verified && (
           <>
             <hr />
