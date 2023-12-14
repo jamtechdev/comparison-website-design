e224d873540fd849f6f2bf46542363ee5b075479
Jamtech Dev <jamtechba@gmail.com>
2023-12-14 12:01:50 +0530

    14-12-23 12:01PM

--- src/app/components/Svg/Questiontool.jsx
+++ src/app/components/Svg/Questiontool.jsx
@@ -6,12 +6,12 @@ const Questiontool = ({ attributes }) => {
       </svg>
       {attributes && (
         <span className="display-content">
-          {attributes?.importance && (
+          {/* {attributes?.importance && (
             <p className="mb-2" style={{ color: "rgb(133, 178, 241)" }}>
               <b style={{ color: "rgb(39 48 78 / 70%)" }}>Importance:{""} </b>
               {""} {attributes?.importance}
             </p>
-          )}
+          )} */}
           {attributes?.description && (
             <p className="mb-2">
               <b>What it is:{""}</b>
