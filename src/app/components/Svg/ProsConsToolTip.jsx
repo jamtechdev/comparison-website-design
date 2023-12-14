7ed50b847a601a75012485092f5a89e7de0f1c8a
Jamtech_Dev <jamtjtplartisan@gmail.com>
2023-12-14 15:02:11 +0530

    removed unused code from prosconstooltip

--- src/app/components/Svg/ProsConsToolTip.jsx
+++ src/app/components/Svg/ProsConsToolTip.jsx
@@ -3,43 +3,6 @@ import { renderToString } from "react-dom/server";
 
 const ProsConsToolTip = (props) => {
   const { hover_phrase, info_not_verified, data } = props;
-  // console.log(hover_phrase, "hover_phrase");
-  // console.log(info_not_verified, "info_not_verified");
-  // console.log(data, "data------------------>>>>>>>>>>>>>>>");
-  function replaceSpecialCharacters(sentence, replacements) {
-    // Define an array of placeholders and their corresponding replacements
-    const placeholders = ["@", "#", "$"];
-
-    // Replace each placeholder with the corresponding value
-    placeholders.forEach((placeholder, index) => {
-      const replacement = replacements[index];
-
-      if (typeof replacement === "string" || typeof replacement === "number") {
-        // If the replacement is a string or number, perform a simple string replacement
-        sentence = sentence?.replace(
-          new RegExp(placeholder, "g"),
-          String(replacement)
-        );
-      } else {
-        // If the replacement is not a string or number, assume it's a JSX element or another type
-        // Replace the placeholder with an empty string and concatenate the replacement
-        sentence = sentence?.split(placeholder).join("") + replacement;
-      }
-    });
-
-    return sentence;
-  }
-
-  // Example usage:
-  const apiString = "@@@ has a runtime of ### which is $$$";
-  const originalSentence = hover_phrase;
-  const newValues = [
-    data?.product_name,
-    data?.value,
-    `is better than ${data?.is_better_than} % of ${data?.attribute_name} and is same as ${data?.is_same_as} % of ${data?.attribute_name}`,
-  ];
-  const updatedSentence = replaceSpecialCharacters(originalSentence, newValues);
-  console.log(updatedSentence, "updatedSentence--->>");
   return (
     <>
       {hover_phrase && (
