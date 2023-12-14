7668ba38afc80b9df8bc39176d349c8a374d70b8
Jamtech Dev <jamtechba@gmail.com>
2023-12-14 11:12:25 +0530

    14-12-23 11:12AM

--- src/app/components/Svg/ProsConsToolTip.jsx
+++ src/app/components/Svg/ProsConsToolTip.jsx
@@ -1,58 +1,50 @@
 import React from "react";
-const ProsConsToolTip = (props) => {
-  const { hover_phrase, info_not_verified, data, typeComp } = props;
-  function replaceSpecialCharacters(sentence, newValues) {
-    // Define an array of special characters to be replaced
-    const specialCharacters = ["@@@", "###", "$$$"];
+import { renderToString } from "react-dom/server";
 
-    // Replace each special character with the corresponding new value
-    specialCharacters.forEach((char, index) => {
-      sentence = sentence?.replace(new RegExp(char, "g"), newValues[index]);
+const ProsConsToolTip = (props) => {
+  const { hover_phrase, info_not_verified, data } = props;
+  // console.log(hover_phrase, "hover_phrase");
+  // console.log(info_not_verified, "info_not_verified");
+  // console.log(data, "data------------------>>>>>>>>>>>>>>>");
+  function replaceSpecialCharacters(sentence, replacements) {
+    // Define an array of placeholders and their corresponding replacements
+    const placeholders = ["@", "#", "$"];
+
+    // Replace each placeholder with the corresponding value
+    placeholders.forEach((placeholder, index) => {
+      const replacement = replacements[index];
+
+      if (typeof replacement === "string" || typeof replacement === "number") {
+        // If the replacement is a string or number, perform a simple string replacement
+        sentence = sentence?.replace(
+          new RegExp(placeholder, "g"),
+          String(replacement)
+        );
+      } else {
+        // If the replacement is not a string or number, assume it's a JSX element or another type
+        // Replace the placeholder with an empty string and concatenate the replacement
+        sentence = sentence?.split(placeholder).join("") + replacement;
+      }
     });
 
     return sentence;
   }
+
+  // Example usage:
+  const apiString = "@@@ has a runtime of ### which is $$$";
   const originalSentence = hover_phrase;
   const newValues = [
     data?.product_name,
     data?.value,
-    `is better than ${data?.is_better_than} % of ${data?.attribute_name} and is same as ${data?.is_same_as} % of ${data?.attribute_name}`,
+    `is better than ${data?.is_better_than} % of ${data?.attribute_name} and is same as ${data?.is_same_as} % of ${data?.attribute_name}`
   ];
   const updatedSentence = replaceSpecialCharacters(originalSentence, newValues);
+  console.log(updatedSentence, "updatedSentence--->>");
   return (
     <>
       <div className="tooltip-display-content">
         <p class="mb-2">
-          {typeComp == "pros" && (
-            <b>
-              {[
-                data?.product_name,
-                data?.value * 100,
-                ` is better than ${
-                  data?.is_better_than ? data?.is_better_than : "N/A"
-                } % of ${
-                  data?.attribute_name ? data?.attribute_name : "N/A"
-                } and is same as ${
-                  data?.is_same_as ? data?.is_same_as * 100 : ""
-                } % of ${data?.attribute_name ? data?.attribute_name : "N/A"}`,
-              ]}
-            </b>
-          )}
-          {typeComp == "cons" && (
-            <b>
-              {[
-                data?.product_name,
-                data?.value * 100,
-                ` is worse than ${
-                  data?.is_better_than ? data?.is_better_than : "N/A"
-                } % of ${
-                  data?.attribute_name ? data?.attribute_name : "N/A"
-                } and is same as ${
-                  data?.is_same_as ? data?.is_same_as * 100 : ""
-                } % of ${data?.attribute_name ? data?.attribute_name : "N/A"}`,
-              ]}
-            </b>
-          )}
+          <b>{updatedSentence}</b>
         </p>
         {info_not_verified && (
           <>
