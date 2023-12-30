33be109bbf1481941d11ee1b73193469a97a5fa1
Jamtech Dev <100343101+mohit-nigam77@users.noreply.github.com>
2023-12-30 10:42:18 +0530

    general marks 6:49Pm

--- src/app/components/Common/BlogSlider/blogSlider.jsx
+++ src/app/components/Common/BlogSlider/blogSlider.jsx
@@ -64,7 +64,7 @@ export default function BlogSlider({ blogData, blogPageType, blogDataList }) {
                         height={0}
                         sizes="100%"
                         alt=""
-                      />
+                      />  
                       <p className="dates">{item.published_at}</p>
                     </div>
                     <span className="blog-title">{item.title}</span>
