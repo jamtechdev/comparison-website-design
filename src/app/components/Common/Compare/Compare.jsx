69f4c04e8da2ec7797aeb8b81c6c3edbed4b4e0e
Jamtech_Dev <jamtjtplartisan@gmail.com>
2023-12-30 18:33:51 +0530

    compare point -1

--- src/app/components/Common/Compare/Compare.jsx
+++ src/app/components/Common/Compare/Compare.jsx
@@ -12,32 +12,33 @@ export default function ComparisonsSlider() {
   const [product2Filled, setProduct2Filled] = useState(false);
   const [search, setSearch] = useState("");
   const [search2, setSearch2] = useState("");
+  const [search3, setSearch3] = useState("");
+
   const [catId, setCatId] = useState();
+  const [catId3, setCatId3] = useState();
+
 
   const [isFocused, setIsFocused] = useState(false);
   const [receivedValue, setReceivedValue] = useState("");
   const [receivedValue2, setReceivedValue2] = useState("");
+  const [receivedValue3, setReceivedValue3] = useState("");
+
 
   console.log(receivedValue, "receivedValue-->>>");
+  console.log(receivedValue2, "receivedValue2-->>>");
 
-  useEffect(() => {
-    if (search2 != "" && search == "") {
-      homePage
-        .getAllSearchedProductsByCategory(catId, search2)
-        .then((res) => {
-          console.log(res, "res-->>>>>>>>");
-          // setFilteredData(res.data.data);
-        })
-        .catch((error) => {
-          console.error("Error fetching data:", error);
-        });
-    }
-  }, [search2]);
-
+  // Function to receive value from child component
+  const handleChildValue2 = (value) => {
+    setReceivedValue2(value);
+  };
+  const handleChildValue3 = (value) => {
+    setReceivedValue3(value);
+  };
   // Function to receive value from child component
   const handleChildValue = (value) => {
     setReceivedValue(value);
   };
+
   const handleProduct1Click = (e) => {
     // Logic to fill the 1st product
     setSearch(e.target.value);
@@ -58,8 +59,8 @@ export default function ComparisonsSlider() {
   const handleProduct3Click = (e) => {
     // Only allow clicking on the 3rd product if the 2nd product is filled
     if (product2Filled) {
-      setSearch(e.target.value);
-
+      setSearch3(e.target.value);
+      setCatId3(receivedValue2.category_id);
       // Logic to fill the 3rd product
       // setProduct3Filled(true);
     }
@@ -89,38 +90,77 @@ export default function ComparisonsSlider() {
           {receivedValue && (
             <Form.Control readOnly value={receivedValue?.name} />
           )}
-          <SearchList
-            compareProSearchList={search}
-            compareTabType={"comparetab"}
-            isFocused={isFocused}
-            setIsFocused={setIsFocused}
-            onSendValue={handleChildValue}
-          />
-          <Image src="/images/vs.svg" width={118} height={40} alt="" />
-          <Form.Control
-            type="text"
-            placeholder="2nd product..."
-            onChange={handleProduct2Click}
-            disabled={!product1Filled}
-            onFocus={() => setIsFocused(true)}
-            onBlur={handleBlur}
-            aria-label="Search"
-            value={search2}
-          />
-          <SearchList
-            compareProSearchList={search2}
-            compareTabType={"comparetab"}
-            isFocused={isFocused}
-            setIsFocused={setIsFocused}
-            onSendValue={handleChildValue}
-          />
-          <Image src="/images/vs.svg" width={118} height={40} alt="" />
-          <Form.Control
-            type="text"
-            placeholder="3rd product... (optional)"
-            onChange={handleProduct3Click}
-            disabled={!product2Filled}
-          />
+          {!receivedValue && (
+            <>
+              <SearchList
+                compareProSearchList={search}
+                compareTabType={"comparetab"}
+                isFocused={isFocused}
+                setIsFocused={setIsFocused}
+                onSendValue={handleChildValue}
+              />
+              <Image src="/images/vs.svg" width={118} height={40} alt="" />
+            </>
+          )}
+          {!receivedValue2 && (
+            <Form.Control
+              type="text"
+              placeholder="2nd product..."
+              onChange={handleProduct2Click}
+              disabled={!product1Filled}
+              onFocus={() => setIsFocused(true)}
+              onBlur={handleBlur}
+              aria-label="Search"
+              value={search2}
+            />
+          )}
+          {!receivedValue2 && (
+            <>
+              <SearchList
+                compareProSearchListForCat={search2}
+                compareTabType={"comparetab"}
+                isFocused={isFocused}
+                setIsFocused={setIsFocused}
+                onSendValue2={handleChildValue2}
+                catId={catId}
+              />
+              <Image src="/images/vs.svg" width={118} height={40} alt="" />
+            </>
+          )}
+
+          {receivedValue2 && (
+            <Form.Control readOnly value={receivedValue2?.name} />
+          )}
+        
+          {!receivedValue3 && (
+            <Form.Control
+              type="text"
+              placeholder="3rd product... (optional)"
+              onChange={handleProduct3Click}
+              disabled={!product2Filled}
+              onFocus={() => setIsFocused(true)}
+              onBlur={handleBlur}
+              aria-label="Search"
+              value={search3}
+            />
+          )}
+          {!receivedValue3 && (
+            <>
+              <SearchList
+                compareProSearchListForCat3={search2}
+                compareTabType={"comparetab"}
+                isFocused={isFocused}
+                setIsFocused={setIsFocused}
+                onSendValue3={handleChildValue3}
+                catId3={catId3}
+              />
+              <Image src="/images/vs.svg" width={118} height={40} alt="" />
+            </>
+          )}
+
+          {receivedValue3 && (
+            <Form.Control readOnly value={receivedValue3?.name} />
+          )}
         </Form>
       </div>
       <div className="text-center">
--- src/app/components/Search/SearchList.jsx
+++ src/app/components/Search/SearchList.jsx
@@ -7,23 +7,52 @@ const SearchList = ({
   isFocused,
   compareProSearchList,
   compareTabType,
-  onSendValue
+  onSendValue,
+  onSendValue2,
+  onSendValue3,
+
+  search2,
+  search3,
+
+  compareProSearchListForCat,
+  compareProSearchListForCat3,
+
+  catId,
+  catId3,
 }) => {
   console.log(compareProSearchList, "compareProSearchList--->>");
   console.log(search, "search minre---->>>");
   const [filteredData, setFilteredData] = useState([]);
   const [filteredProData, setFilteredProData] = useState([]);
-  const [selectedText,setSelectedText]=useState();
+  const [filteredProData2, setFilteredProData2] = useState([]);
+  const [filteredProData3, setFilteredProData3] = useState([]);
 
+  const [selectedText, setSelectedText] = useState();
 
-  const [childValue, setChildValue] = useState('');
+  const [childValue, setChildValue] = useState("");
+  const [childValue2, setChildValue2] = useState("");
+  const [childValue3, setChildValue3] = useState("");
+
+  console.log(search, "test1-------->>>>>>>>>");
+  console.log(compareProSearchListForCat, "test2-------->>>>>>>>>");
 
   const handleChange = (item) => {
     // let value = event.target.outerText;
-    console.log(item, "l------>>>>>>>");
     setChildValue(item.name);
     // Send the value to the parent component
     onSendValue(item);
+  };
+  const handleChange2 = (item) => {
+    // let value = event.target.outerText;
+    setChildValue2(item.name);
+    // Send the value to the parent component
+    onSendValue2(item);
+  };
+  const handleChange3 = (item) => {
+    // let value = event.target.outerText;
+    setChildValue3(item.name);
+    // Send the value to the parent component
+    onSendValue3(item);
   };
   useEffect(() => {
     if (search !== "" || search !== undefined) {
@@ -51,11 +80,40 @@ const SearchList = ({
     }
   }, [compareProSearchList]);
 
+  useEffect(() => {
+    if (compareProSearchListForCat != "" || search2 != "") {
+      homePage
+        .getAllSearchedProductsByCategory(catId, compareProSearchListForCat)
+        .then((res) => {
+          console.log(res, "res-->>");
+          setFilteredProData2(res.data.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    }
+  }, [compareProSearchListForCat, catId]);
+
+  useEffect(() => {
+    if (compareProSearchListForCat3 != "" || search3 != "") {
+      homePage
+        .getAllSearchedProductsByCategory(catId3, compareProSearchListForCat3)
+        .then((res) => {
+          console.log(res, "res-->>");
+          setFilteredProData3(res.data.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    }
+  }, [compareProSearchListForCat3, catId3]);
+
   const capitalizeFirstLetter = (str) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
   };
 
   console.log(filteredProData, "filteredProData-->>>");
+  console.log(filteredProData2, "filteredProData2");
 
   const paramsCate = (category, links) => {
     if (category === "guides") {
@@ -70,7 +128,16 @@ const SearchList = ({
   console.log(selectedText, "selectedText-->>>>>>");
   return (
     <>
-      <div className={isFocused && search?.length > 0  || isFocused && compareProSearchList?.length >0 ? "" : "d-none"}>
+      <div
+        className={
+          (isFocused && search?.length > 0) ||
+          (isFocused && compareProSearchList?.length > 0) ||
+          (isFocused && compareProSearchListForCat?.length > 0) ||
+          (isFocused && compareProSearchListForCat3?.length > 0)
+            ? ""
+            : "d-none"
+        }
+      >
         <div className="search-dropdown-list">
           {filteredData &&
             Object.keys(filteredData).map((category, index) => (
@@ -112,7 +179,12 @@ const SearchList = ({
           {filteredProData &&
             filteredProData.map((item, index) => (
               <div className="search-data-list" key={index}>
-                <h2 className="search-data-heading" onClick={(e)=>{handleChange(item)}}>
+                <h2
+                  className="search-data-heading"
+                  onClick={(e) => {
+                    handleChange(item);
+                  }}
+                >
                   {capitalizeFirstLetter(item?.name)}
                 </h2>
                 {/* <ul>
@@ -135,11 +207,76 @@ const SearchList = ({
                 </ul> */}
               </div>
             ))}
-          {filteredProData =="" || search == "" && (
-            <div className="search-data-list">
-              <span className="no-result-found">No results found</span>
-            </div>
-          )}
+          {filteredProData2 &&
+            filteredProData2.map((item, index) => (
+              <div className="search-data-list" key={index}>
+                <h2
+                  className="search-data-heading"
+                  onClick={(e) => {
+                    handleChange2(item);
+                  }}
+                >
+                  {capitalizeFirstLetter(item?.name)}
+                </h2>
+                {/* <ul>
+                  <Link
+                    href="#"
+               
+                  >
+                    <li>
+                      <span>{item?.name}</span>
+
+                      {item?.main_image && (
+                        <img
+                          src={item?.main_image}
+                          alt={`Image ${itemIndex}`}
+                        />
+                      )}
+                    </li>
+                  </Link>
+                 
+                </ul> */}
+              </div>
+            ))}
+          {filteredProData3 &&
+            filteredProData3.map((item, index) => (
+              <div className="search-data-list" key={index}>
+                <h2
+                  className="search-data-heading"
+                  onClick={(e) => {
+                    handleChange3(item);
+                  }}
+                >
+                  {capitalizeFirstLetter(item?.name)}
+                </h2>
+                {/* <ul>
+                  <Link
+                    href="#"
+               
+                  >
+                    <li>
+                      <span>{item?.name}</span>
+
+                      {item?.main_image && (
+                        <img
+                          src={item?.main_image}
+                          alt={`Image ${itemIndex}`}
+                        />
+                      )}
+                    </li>
+                  </Link>
+                 
+                </ul> */}
+              </div>
+            ))}
+          {filteredProData == "" ||
+            search == "" ||
+            filteredProData2 == "" ||
+            (filteredProData3 && (
+              <div className="search-data-list">
+                <span className="no-result-found">No results found</span>
+              </div>
+            ))}
         </div>
       </div>
     </>
